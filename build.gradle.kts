93ca679ccc8e981e263058339244eabff9701679
mkh9293 <mkh9293@gmail.com>
2024-07-31 22:59:10 +0900

    feat: Configure multi module project with latest version of kotlin and spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
-    id("java")
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0" apply false
+    id("org.springframework.boot") version "3.3.2" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+allprojects {
+    group = "com.playground"
 
-repositories {
-    mavenCentral()
+    repositories {
+        mavenCentral()
+    }
+
+    // Settings for `KotlinCompile` tasks
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17) // This option specifies the target version of the generated JVM bytecode
+            freeCompilerArgs.add("-Xjsr305=strict") // `-Xjsr305=strict` enables the strict mode for JSR-305 annotations
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+subprojects {
+    apply {
+        plugin("kotlin")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    dependencies {
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Jackson extensions for Kotlin for working with JSON
+        implementation("org.jetbrains.kotlin:kotlin-reflect") // Kotlin reflection library, required for working with Spring
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,7 @@
+pluginManagement {
+    plugins {
+        kotlin("jvm") version "2.0.0"
+    }
+}
 rootProject.name = "Pay-Playground"
-
+include("membership")
